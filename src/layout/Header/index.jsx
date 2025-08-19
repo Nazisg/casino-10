0e1381ddba9a52180e6dad7f1468250ec46653d7
Nazrin Isgandarova <isgnezrin@gmail.com>
2025-08-20 03:57:56 +0400

    initial

---
+++ src/layout/Header/index.jsx
@@ -0,0 +1,68 @@
+"use client";
+import { useState, useEffect } from "react";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+export default function Header() {
+  const [open, setOpen] = useState(false);
+  const pathname = usePathname();
+
+  // Auto-close sidebar when the route changes
+  useEffect(() => {
+    setOpen(false);
+  }, [pathname]);
+
+  return (
+    <>
+      {/* HEADER */}
+      <header className="fixed top-0 left-0 w-full bg-[#260126] z-50 shadow-lg">
+        <nav className="relative max-w-7xl mx-auto flex items-center justify-between py-4 px-6">
+          {/* Logo */}
+          <Link
+            href="/"
+            className="text-xl font-bold text-white cursor-pointer flex gap-2 items-center"
+          >
+            <img src="/icons/casino-logo.webp" alt="logo" className="w-8" />
+            <h2 className="glow-text">CASINO</h2>
+          </Link>
+
+          {/* Desktop Nav */}
+          <ul className=" hidden lg:flex space-x-8 font-bold text-white">
+            <li><Link href="/top-casinos" className="hover:text-[#FFD600]">Top Casinos</Link></li>
+            <li><Link href="/about-us" className="hover:text-[#FFD600]">About Us</Link></li>
+            <li><Link href="/terms" className="hover:text-[#FFD600]">Terms & Conditions</Link></li>
+            <li><Link href="/privacy-policy" className="hover:text-[#FFD600]">Privacy Policy</Link></li>
+            <li><Link href="/responsible-gaming" className="hover:text-[#FFD600]">Responsible Gaming</Link></li>
+          </ul>
+
+          {/* Burger / X button for mobile */}
+          <button
+            className="lg:hidden z-50 cursor-pointer"
+            onClick={() => setOpen(!open)}
+          >
+            {open ? (
+              <img src="/icons/close.png" alt="close" className="w-4 h-4" />
+            ) : (
+              <img src="/icons/burger-menu.svg" alt="burger-menu" />
+            )}
+          </button>
+        </nav>
+      </header>
+
+      {/* SIDE MENU */}
+      <div
+        className={`fixed z-40 top-0 right-0 h-full w-64 bg-[#260126] text-white p-6 transform transition-transform duration-300 shadow-lg ${
+          open ? "translate-x-0" : "translate-x-full"
+        }`}
+      >
+        <ul className="mt-12 space-y-4 font-bold">
+          <li><Link href="/top-casinos">Top Casinos</Link></li>
+          <li><Link href="/about-us">About Us</Link></li>
+          <li><Link href="/terms">Terms & Conditions</Link></li>
+          <li><Link href="/privacy-policy">Privacy Policy</Link></li>
+          <li><Link href="/responsible-gaming">Responsible Gaming</Link></li>
+        </ul>
+      </div>
+    </>
+  );
+}
